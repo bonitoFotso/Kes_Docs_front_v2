070ffa5f1607c0f3d5bb6267494f4e8acb9b6492
bonitoFotso <bonitofotso55@gmail.com>
2024-11-14 11:28:13 +0100

    proces.env 2

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,7 +6,7 @@ import axios from 'axios';
 import { getAuth, removeAuth } from '../app/modules/auth';
 
 // Récupérer l'URL de l'API depuis le fichier .env
-const API_URL = process.env.VITE_APP_API_URL || 'http://127.0.0.1:8000';
+const API_URL = process.env.VITE_APP_API_URL ;
 
 // Instance principale d'Axios pour les requêtes avec authentification (JSON)
 const apiClient: AxiosInstance = axios.create({
